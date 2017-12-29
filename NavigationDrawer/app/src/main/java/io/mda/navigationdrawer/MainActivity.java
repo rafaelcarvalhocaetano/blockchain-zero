94a40c4f814369edb16ade8565552a598e22bf9f
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-29 15:18:01 -0200

    Add navigationdrawer

--- NavigationDrawer/app/src/main/java/io/mda/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/io/mda/navigationdrawer/MainActivity.java
@@ -1,5 +1,8 @@
 package io.mda.navigationdrawer;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,14 +10,32 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar( toolbar );
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener( toggle );
+
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen( GravityCompat.START ) ){
+            drawerLayout.closeDrawer( GravityCompat.START );
+        }else {
+            super.onBackPressed();
+        }
 
     }
 }
