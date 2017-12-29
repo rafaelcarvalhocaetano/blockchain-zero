55d9b8640dac1256854ec8c92acd4388a03d5688
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-29 20:06:16 -0200

    App com circular progressbar e progressbar simples

--- ProgressBar/app/src/main/java/io/mda/progressbar/MainActivity.java
+++ ProgressBar/app/src/main/java/io/mda/progressbar/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ProgressBar;
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
+    private ProgressBar linearBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         progressBar = (ProgressBar) findViewById(R.id.circular_bar);
+        linearBar = (ProgressBar) findViewById(R.id.linear_bar);
         progressBar.setVisibility(View.GONE);
+
+        //setando o valor no linear progress bar de 40
+        int init = 0;
+        int max = 100;
+        linearBar.setProgress( init );
+        linearBar.setSecondaryProgress( init );
+        linearBar.setMax( max );
 
         new AsyncCircular().execute();
 
@@ -29,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
             for (int i=0; i<=100; i++){
                 try {
+                    //chamando o método onProgressUpdate
+                    publishProgress( i );
+
                     Thread.sleep(300);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -45,6 +57,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            linearBar.setProgress( values[0] );
+            linearBar.setProgress( values[0] + 15 );
         }
     }
 }
