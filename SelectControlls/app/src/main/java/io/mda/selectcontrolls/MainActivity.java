d1221ee711be95364ee4cd4c617d28f711ebe475
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-29 16:02:47 -0200

    App de Selects - RadioGroup, Switch, CheckBox

--- SelectControlls/app/src/main/java/io/mda/selectcontrolls/MainActivity.java
+++ SelectControlls/app/src/main/java/io/mda/selectcontrolls/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.SwitchCompat;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private SwitchCompat switchCompat;
     private AppCompatCheckBox appCompatCheckBox;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +43,25 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "MARCADO", Toast.LENGTH_LONG).show();
                 }else{
                     Toast.makeText(getApplicationContext(), "DESMARCADO", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId){
+                    case R.id.option1:
+                        Toast.makeText(getApplicationContext(), "OPÇÂO 1", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.option2:
+                        Toast.makeText(getApplicationContext(), "OPÇÂO 2", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.option3:
+                        Toast.makeText(getApplicationContext(), "OPÇÂO 3", Toast.LENGTH_LONG).show();
+                        break;
                 }
             }
         });
