465c90c0f92b08b7a629394af95976e74b52b46a
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-29 15:30:00 -0200

    Add navigationdrawer

--- NavigationDrawer/app/src/main/java/io/mda/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/io/mda/navigationdrawer/MainActivity.java
@@ -1,21 +1,27 @@
 package io.mda.navigationdrawer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -27,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
         toggle.syncState();
 
+        //colocando click no menu do drawer
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener( this );
+
+
     }
 
     @Override
@@ -37,5 +48,29 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.menu_inbox:
+                Toast.makeText(this, "INBOX", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_favoritos:
+                Toast.makeText(this, "FAVORITOS", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_enviados:
+                Toast.makeText(this, "EMAILS", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_lixeira:
+                Toast.makeText(this, "LIXEIRA", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_span:
+                Toast.makeText(this, "SPAN", Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawerLayout.closeDrawer( GravityCompat.START );
+        return false;
     }
 }
