eb2d437489d05225226a77089171f99b738b0b0f
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-31 01:09:04 -0200

    Add App RecycleView

--- RecicleView/app/src/main/java/io/mda/recicleview/MainActivity.java
+++ RecicleView/app/src/main/java/io/mda/recicleview/MainActivity.java
@@ -2,12 +2,52 @@ package io.mda.recicleview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.pkmmte.pkrss.Article;
+import com.pkmmte.pkrss.Callback;
+import com.pkmmte.pkrss.PkRSS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Callback {
+
+
+    private RecyclerView recyclerView;
+    private RecycleAdapter recycleAdapter;
+    private List<Article> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycleview);
+       recyclerView.setLayoutManager( new LinearLayoutManager(this));
+
+       recycleAdapter = new RecycleAdapter(list);
+       recyclerView.setAdapter( recycleAdapter );
+
+        PkRSS.with(this).load("http://www.androidpro.com.br/feed/").skipCache().callback(this).async();
+    }
+
+    @Override
+    public void onPreload() {
+
+    }
+
+    @Override
+    public void onLoaded(List<Article> newArticles) {
+        list.clear();
+        list.addAll( newArticles );
+        recycleAdapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onLoadFailed() {
+
     }
 }
