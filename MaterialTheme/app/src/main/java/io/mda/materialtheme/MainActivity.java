9bc3fa636606277642fe6ce0b0cf620e13f122d6
Rafael Carvalho <rapha.pse@outlook.com>
2022-01-09 19:12:43 -0300

    Clean

--- MaterialTheme/app/src/main/java/io/mda/materialtheme/MainActivity.java
+++
@@ -1,1 +0,0 @@
-package io.mda.materialtheme;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;public class MainActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);    }}
--- README.md
+++
@@ -1,3 +0,0 @@
-# MaterialDesign-Android
-Criando aplicações android material design
-Integrando informações com banco de dados NOSql do firebase e usando autenticação do FB.
